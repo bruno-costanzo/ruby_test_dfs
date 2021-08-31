d44d812a632da65902de3aa057a770c739f21576
bruno-costanzo <dev.bcostanzo@gmail.com>
2021-08-31 19:39:33 -0300

    solution

--- ruby_test_dfs.rb
+++ ruby_test_dfs.rb
@@ -12,7 +12,7 @@ COL_SIZE = input.size
 DIR = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
 def count_figures(matrix)
-  visited = Array.new(ROW_SIZE) { Array.new(COL_SIZE) {false}}
+  visited = Array.new(ROW_SIZE) { Array.new(COL_SIZE) { false } }
 
   figure_lenght = 0
 
@@ -20,47 +20,39 @@ def count_figures(matrix)
 
   result = []
 
-  
   matrix.each_with_index do |row, x|
-    row.each_with_index do |element, y|
-      if matrix[x][y] == 1 && !visited[x][y]
-        figure_lenght += 1
-        figure_lenght += dfs(matrix, x, y, visited)
+    row.each_with_index do |_element, y|
+      next unless matrix[x][y] == 1 && !visited[x][y]
 
-        result << figure_lenght
-        figure_lenght = 0
-      end
+      figure_lenght += 1
+      figure_lenght += dfs(matrix, x, y, visited)
 
-       
+      result << figure_lenght
+      figure_lenght = 0
     end
   end
-  
+
   result.sort
 end
 
-def valid? (matrix, row, col, visited)
-  (row >= 0) && (row < ROW_SIZE) && (col >= 0) && (col < COL_SIZE) && (matrix[row][col] == 1 && !visited[row][col]);
+def valid?(matrix, row, col, visited)
+  (row >= 0) && (row < ROW_SIZE) && (col >= 0) && (col < COL_SIZE) && (matrix[row][col] == 1 && !visited[row][col])
 end
 
-def dfs (matrix, row, col, visited, tail = 0)
+def dfs(matrix, row, col, visited, tail = [])
   rowNbr = [0, 0, 1, -1]
   colNbr = [1, -1, 0, 0]
 
-  visited[row][col] = true;
+  visited[row][col] = true
 
-  4.times do | i |
-    if valid?(matrix, row + rowNbr[i], col + colNbr[i], visited)
-      tail += 1
-      dfs(matrix, row + rowNbr[i], col + colNbr[i], visited, tail)
-      
-    end
+  4.times do |i|
+    next unless valid?(matrix, row + rowNbr[i], col + colNbr[i], visited)
 
+    tail << 1
+    dfs(matrix, row + rowNbr[i], col + colNbr[i], visited, tail)
   end
 
-  tail
+  tail.size
 end
-
-
-
 
 p count_figures(input)
